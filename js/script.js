45da48bbc65274beb441d4ea1b8975464533f173
rayxxsuzuya <56367629+rayxxsuzuya@users.noreply.github.com>
2021-05-08 02:49:47 +0300

    header & delivery adaptive

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ $(function() {
 		arrows: true
 	});
 });
+
+var menuBtn = document.querySelector('.adaptive');
+var menuLines = document.querySelectorAll('.adaptive__line');
+var menu = document.querySelector('.adaptive-menu');
+
+menuBtn.onclick = function() {
+
+	for (var i = 0; i < menuLines.length; i++) {
+		menuLines[i].classList.toggle('active');
+	}
+
+	menu.classList.toggle('active');
+}
