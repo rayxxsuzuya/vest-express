34ff179164ccf7e9d89f028a6b01340c62abdeed
rayxxsuzuya <56367629+rayxxsuzuya@users.noreply.github.com>
2021-05-08 05:39:55 +0300

    finished

--- js/script.js
+++ js/script.js
@@ -4,15 +4,31 @@ $(function() {
 	});
 });
 
-var menuBtn = document.querySelector('.adaptive');
-var menuLines = document.querySelectorAll('.adaptive__line');
-var menu = document.querySelector('.adaptive-menu');
+if (document.querySelector(".header-second") !== null) {
+  var s_menuBtn = document.querySelector(".s-adaptive");
+  var s_menuLines = document.querySelectorAll(".s-adaptive__line");
+  var s_menu = document.querySelector(".s-adaptive-menu");
 
-menuBtn.onclick = function() {
+  s_menuBtn.onclick = function () {
+    for (var j = 0; j < s_menuLines.length; j++) {
+      s_menuLines[j].classList.toggle("active");
+    }
 
-	for (var i = 0; i < menuLines.length; i++) {
-		menuLines[i].classList.toggle('active');
-	}
-
-	menu.classList.toggle('active');
+    s_menu.classList.toggle("active");
+  };
 }
+
+if (document.querySelector('header') !== null) {
+	var menuBtn = document.querySelector(".adaptive");
+  var menuLines = document.querySelectorAll(".adaptive__line");
+  var menu = document.querySelector(".adaptive-menu");
+
+  menuBtn.onclick = function () {
+    for (var i = 0; i < menuLines.length; i++) {
+      menuLines[i].classList.toggle("active");
+    }
+
+    menu.classList.toggle("active");
+  };
+}
+
